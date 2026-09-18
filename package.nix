272ebfc1d25926e765ffd06dc16ea780d176dd37
David Vanderklay <davidvanderklay@gmail.com>
2026-09-18 04:08:43 -0500

    fix: install limusic desktop entry

--- package.nix
+++ package.nix
@@ -2,23 +2,42 @@
   appimageTools,
   fetchurl,
   libappindicator-gtk3,
+  symlinkJoin,
   webkitgtk_4_1,
 }:
 
 let
   version = "0.7.3";
-in
-appimageTools.wrapType2 {
-  pname = "limusic";
-  inherit version;
-
   src = fetchurl {
     url = "https://github.com/SimoHypers/limusic/releases/download/v${version}/limusic_${version}_amd64.AppImage";
     hash = "sha256-RDwXrGkqZ8BvFiEBKKt27BA4saRhzbJUXdkQ6k+KFvw=";
   };
+  contents = appimageTools.extract {
+    pname = "limusic";
+    inherit version src;
+  };
+  runtime = appimageTools.wrapAppImage {
+    pname = "limusic";
+    inherit version contents;
 
-  extraPkgs = appimagePkgs: with appimagePkgs; [
-    webkitgtk_4_1
-    libappindicator-gtk3
-  ];
+    extraPkgs = appimagePkgs: with appimagePkgs; [
+      webkitgtk_4_1
+      libappindicator-gtk3
+    ];
+  };
+in
+symlinkJoin {
+  name = "limusic-${version}";
+  paths = [ runtime ];
+
+  postBuild = ''
+    install -Dm444 ${contents}/usr/share/applications/limusic.desktop \
+      $out/share/applications/limusic.desktop
+    substituteInPlace $out/share/applications/limusic.desktop \
+      --replace-fail 'Exec=limusic-app' 'Exec=limusic' \
+      --replace-fail 'Name=limusic' 'Name=Limusic'
+
+    mkdir -p $out/share/icons
+    cp -R ${contents}/usr/share/icons/hicolor $out/share/icons/
+  '';
 }
